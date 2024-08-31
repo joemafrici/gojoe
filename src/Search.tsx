85463f6fab0c7ce4fa309efbbdba6a9e0cdd3f41
Joe Mafrici <mac@joemafrici.com>
2024-08-31 10:43:02 -0700

    make fetch request to server for search results

--- src/Search.tsx
+++ src/Search.tsx
@@ -1,12 +1,26 @@
-
+import { useState } from 'react'
 const Search: React.FC = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(`https://localhost:7878/search?query=${searchQuery}`);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      const json = await response.json();
+      console.log(json);
+    } catch (err) {
+      console.error(err);
+    }
+  };
   return (
     <>
-      <label>Enter search query</label>
-      <input type='text'></input>
-      <ul>
-        <li>Search Engine</li>
-      </ul>
+      <form method='POST' onSubmit={handleSubmit}>
+        <label htmlFor='searchQueryInput'>Enter search query</label>
+        <input id='searchQueryInput' name='searchQueryInput' type='text' value={searchQuery} onChange={e => setSearchQuery(e.target.value)}></input>
+        <button type='submit' >Submit</button>
+      </form>
     </>
   );
 };
