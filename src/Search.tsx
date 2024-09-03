3775267ef5fdc747f9543ceedba1af00f56812f3
Joe Mafrici <mac@joemafrici.com>
2024-09-02 17:41:02 -0700

    display search results

--- src/Search.tsx
+++ src/Search.tsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
+interface SearchResult {
+  filename: string;
+  similarity: number;
+  snippets: string[];
+}
 const Search: React.FC = () => {
   const [searchQuery, setSearchQuery] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
-  const listItems = searchResults.map(result => <li>{result}</li>);
-  // hello
+  const [searchResults, setSearchResults] = useState<SearchResult[]>([]);
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
@@ -20,12 +23,24 @@ const Search: React.FC = () => {
   };
   return (
     <>
-      <form method='POST' onSubmit={handleSubmit}>
+      <form method='GET' onSubmit={handleSubmit}>
         <label htmlFor='searchQueryInput'>Enter search query</label>
         <input id='searchQueryInput' name='searchQueryInput' type='text' value={searchQuery} onChange={e => setSearchQuery(e.target.value)}></input>
         <button type='submit' >Submit</button>
       </form>
-      <ul>{listItems}</ul>
+      <ul>{searchResults.map((result, idx) => (
+        <li key={idx}>
+          <h3>{result.filename}</h3>
+          <p>Similarity: {result.similarity.toFixed(4)} </p>
+          <ul>
+            {result.snippets.map((snippet, snippetIdx) => (
+              <li key={snippetIdx}>{snippet}
+              </li>
+            ))}
+          </ul>
+        </li>
+      ))}
+      </ul>
     </>
   );
 };
